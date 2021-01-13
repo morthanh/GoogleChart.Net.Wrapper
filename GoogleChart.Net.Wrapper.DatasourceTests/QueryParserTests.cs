7f3204250ef69414f8bd15d23e47e43826f95ce3
morthanh <morthanh@gmail.com>
2021-01-13 11:04:01 +0100

    beginning of query parser

---
+++ GoogleChart.Net.Wrapper.DatasourceTests/QueryParserTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using GoogleChart.Net.Wrapper.Datasource;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoogleChart.Net.Wrapper.Datasource.Tests
+{
+    [TestFixture()]
+    public class QueryParserTests
+    {
+        [Test()]
+        public void Parse_Select_FindTokens()
+        {
+
+            var parserResult = QueryParser.Parse("select col1, col2  ,col3, col    4");
+
+
+            Assert.IsTrue(parserResult.SelectTokens.Length == 4);
+
+        }
+
+        [Test]
+        public void Parse_Where_FindTokens()
+        {
+
+        }
+    }
+
+}
